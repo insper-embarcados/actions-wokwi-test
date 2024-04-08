fc1f66d377da37401e27526d6a5bd6b6bc73fc25
Your Name <corsiferrao@gmail.com>
2024-04-08 10:00:29 -0300

    test in place

--- test.py
+++ test.py
@@ -1,29 +1,6 @@
 import pytest
 import subprocess
-import shutil
 import os
-import toml
-import subprocess
-
-test_folder = ".test"
-
-
-def copy_files(exe_name):
-    shutil.copyfile(f"{exe_name}/diagram.json", f"{test_folder}/diagram.json")
-    shutil.copyfile(f"{exe_name}/test.yaml", f"{test_folder}/test.yaml")
-
-
-def gen_wokwi_toml(exe_name):
-    base_path = os.path.join("..", "build", exe_name)
-    firmware_path = os.path.join(base_path, f"{exe_name}.uf2")
-    elf_path = os.path.join(base_path, f"{exe_name}.elf")
-
-    data = {"wokwi": {"version": 1, "firmware": firmware_path, "elf": elf_path}}
-    os.makedirs(test_folder, exist_ok=True)
-    toml_file_path = os.path.join(test_folder, "wokwi.toml")
-    with open(toml_file_path, "w") as toml_file:
-        toml.dump(data, toml_file)
-
 
 def run_build(name):
     if not os.path.isdir("build"):
@@ -38,6 +15,7 @@ def run_build(name):
         )
     else:
         os.chdir("build")
+        
     subprocess.run(
         f"make {name}",
         shell=True,
@@ -48,19 +26,10 @@ def run_build(name):
     os.chdir("..")
 
 
-def run_test(name):
+def test_code(name):
     print("Building projects")
     run_build(name)
-    gen_wokwi_toml(name)
-    copy_files(name)
-    os.chdir(test_folder)
-    command = "wokwi-cli --timeout 5000 --scenario test.yaml"
+    command = f"wokwi-cli --timeout 5000 --scenario test.yaml {name}"
     process = subprocess.run(command, shell=True, stderr=subprocess.PIPE, text=True)
-    os.chdir("..")
-    if process.returncode != 0:
-        return 1
-    return 0
+    assert process.returncode == 0
 
-
-def test_code(name):
-    assert run_test(name) == 0
