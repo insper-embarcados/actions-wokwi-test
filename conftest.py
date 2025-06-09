d8afa85797be4a0d6b5e7076bd816f3ead73ca5a
Your Name <corsiferrao@gmail.com>
2025-06-09 15:00:47 -0300

    fix fixture

--- conftest.py
+++ conftest.py
@@ -7,15 +7,18 @@ def pytest_addoption(parser):
     parser.addoption("--name", action="store")
     parser.addoption("--timeout", action="store")
 
-
 @pytest.fixture(scope="session")
 def name(request):
     name_value = request.config.option.name
     if name_value is None:
-        pytest.skip()
+        pytest.skip("O parâmetro --name é obrigatório.")
     return name_value
 
+@pytest.fixture(scope="session")
+def timeout(request):
     timeout_value = request.config.option.timeout
     if timeout_value is None:
-        timeout_value = 10000
+        timeout_value = 10000  # Valor padrão
+    else:
+        timeout_value = int(timeout_value)
     return timeout_value
